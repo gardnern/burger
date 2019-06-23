2a41f52c3274db7db66de6242b2de3beb1804ed5
gardnern <48269958+gardnern@users.noreply.github.com>
2019-06-23 16:16:48 -0600

    updated orm.js, burger.js and burger_controller.js

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('pizza-burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('yum-burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('moss-burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese-burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('buffalo-burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('chicken-burger', false);
