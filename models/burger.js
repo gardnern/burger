9f87fac9c371c850b15ebad1589aa683906650e9
gardnern <48269958+gardnern@users.noreply.github.com>
2019-06-29 14:02:11 -0600

    updated

--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,29 @@
-//importing the ORM to create the functions that will interact with the database
+// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 var burger = {
-    selectAll: function (cb) {
-        orm.selectAll(function (res) {
-            cb(res);
-        });
-    },
-    insertOne: function (burger, cb) {
-        orm.insertOne(burger, function (res) {
-            cb(res);
-        });
-    },
-    updateOne: function (id, cb) {
-        orm.updateOne([id], function (res) {
-            cb(res);
-        });
-    }
+  all: function(cb) {
+    orm.all("burger", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burger", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burger", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burger", condition, function(res) {
+      cb(res);
+    });
+  }
 };
 
+// Export the database functions for the controller (burger Controller.js).
 module.exports = burger;
